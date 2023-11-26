d60fe343e2c47d5c3eab3042a33daba68c4d037f
leafarsize <152042162+leafarsize@users.noreply.github.com>
2023-11-25 18:36:43 -0700

    ola

--- src/Seguros/Main.java
+++ src/Seguros/Main.java
@@ -7,5 +7,6 @@ package Seguros;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Saludos Nuevamente");
+        System.out.println("BorrayoPvto");
     }
 }
