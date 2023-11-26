018192dc0b4cd2973cd8d2425ccab12fba3b5e2f
Seguros <dax.saanchez@gmail.com>
2023-11-26 00:50:15 -0700

    try

--- src/conexionBD/BaseConexion.java
+++ src/conexionBD/BaseConexion.java
@@ -2,14 +2,7 @@ package conexionBD;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import javax.swing.JOptionPane;
 import leer.Leer;
 
 /**
