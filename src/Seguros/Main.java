3da50248f544b00b14770945d22357f1a74e8d82
leafarsize <152042162+leafarsize@users.noreply.github.com>
2023-11-25 18:35:37 -0700

    Primerop
    ola

--- src/Seguros/Main.java
+++ src/Seguros/Main.java
@@ -6,6 +6,6 @@ package Seguros;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("PRUEBA");
+        System.out.println("Saludos Nuevamente");
     }
 }
