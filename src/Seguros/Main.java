6c48776c3dc830ac0e2fc9b22cd9a2dd53d34a58
leafarsize <152042162+leafarsize@users.noreply.github.com>
2023-11-25 18:38:10 -0700

    otro

--- src/Seguros/Main.java
+++ src/Seguros/Main.java
@@ -8,5 +8,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Saludos Nuevamente");
         System.out.println("BorrayoPvto");
+        System.out.println("ObedPVto");
     }
 }
