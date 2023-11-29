801bc846dc46d6136cbaeb1e00575c140ddea957
Seguros <dax.saanchez@gmail.com>
2023-11-28 19:48:52 -0700

    Validaciones para el cliente

--- src/Persona/ClienteLeal.java
+++ src/Persona/ClienteLeal.java
@@ -10,6 +10,10 @@ public class ClienteLeal extends Cliente {
 
     public ClienteLeal(String nombre, String apellido, String numTelefonon, int antiguedad) {
         super(nombre, apellido, numTelefonon, antiguedad);
+        while (super.getAntiguedad() < 10) {
+            Leer.showDatos("La antigüedad para este cliente va de 10 años en adelante");
+            super.setAntiguedad(Leer.leerEntero("Ingrese una antiguedad con estos rangos:"));
+        }
     }
 
     @Override
