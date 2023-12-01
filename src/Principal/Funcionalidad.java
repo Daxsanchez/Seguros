6055c4339fb9ca66031561448485724210100bfb
Seguros <dax.saanchez@gmail.com>
2023-12-01 10:52:43 -0700

    Cmabios

--- src/Principal/Funcionalidad.java
+++ src/Principal/Funcionalidad.java
@@ -17,21 +17,37 @@ import Persona.AgenteDeSeguros;
 import Persona.AnalistaRiesgos;
 import Persona.Empleado;
 import Persona.ProcesarEmpleado;
+import vehiculos.Vehiculo;
 
 /**
  *
  * @author Dax Sánchez
  */
 public class Funcionalidad {
-    
+
     public void funcionesCliente() {
         ProcesarCliente clientes = new ProcesarCliente();
-        
+
         Cliente[] cliente = clientes();
         clientes.showClientes(cliente);
-        
+
     }
-    
+
+    public void funcionesEmpleado() {
+        ProcesarEmpleado p = new ProcesarEmpleado();
+        Empleado[] empleados = empleados();
+        p.mostrarEmpleado(empleados);
+
+    }
+
+    private Empleado[] empleados() {
+        Empleado[] empleados = new Empleado[3];
+        empleados[0] = new AgenteDeSeguros(true, 300, 2134142, "Superior", 12000.00, "Arturo", "Bañuelos", "311-424-221");
+        empleados[1] = new AnalistaRiesgos(2636363, "Analista experto", 18000.00, "Miguel", "Miramontes", "311-232-832", 34);
+        empleados[1] = new AgenteDeSeguros(false, 100, 55235225, "Nuevo", 7000.00, "Samuel", "Gomez", "311-534-821");
+        return empleados;
+    }
+
     public Seguro[] seguros() {
         Seguro[] seguros = new Seguro[3];
         seguros[0] = new SeguroColision(5, "10014313", new Date(), new Date(), true,
@@ -42,7 +58,7 @@ public class Funcionalidad {
                 "Seguro contra robo", 6000.0, 50000.0);
         return seguros;
     }
-    
+
     public Renovacion[] renovaciones() {
         Renovacion[] renovaciones = new Renovacion[3];
         Seguro[] seguros = seguros();
@@ -52,7 +68,7 @@ public class Funcionalidad {
         renovaciones[2] = new RenovacionPlus("368", new Date(), seguros[2], "Transferencia", clientes[1]);
         return renovaciones;
     }
-    
+
     public Cliente[] clientes() {
         Cliente[] clientes = new Cliente[3];
         clientes[0] = new ClienteNuevo("Juan", "Sánchez", "31135643", 3);
@@ -60,19 +76,13 @@ public class Funcionalidad {
         clientes[2] = new ClienteLeal("Edgar", "Hernández", "311354565", 15);
         return clientes;
     }
-    
-    public void pruebaEmpleado() {
-        ProcesarEmpleado p = new ProcesarEmpleado();
-        Empleado[] empleados = empleados();
-        p.mostrarEmpleado(empleados);
-        
+
+    public Vehiculo[] vehiculos() {
+        Vehiculo[] vehiculos = new Vehiculo[3];
+        vehiculos[0] = new Vehiculo("RCV-454-ER3", "Toyota", "GR TOYOTA GAZOO RACING");
+        vehiculos[1] = new Vehiculo("PLV-R32-FSD", "Verde", 2002);
+        vehiculos[2] = new Vehiculo("PDA-GR3-G41", "suzuki", "FRONX BOOSTERGREEN ", "Rojo", 2012);
+        return vehiculos;
     }
     
-    private Empleado[] empleados() {
-        Empleado[] empleados = new Empleado[3];
-        empleados[0] = new AgenteDeSeguros(true, 300, 2134142, "Superior", 12000.00, "Arturo", "Bañuelos", "311-424-221");
-        empleados[1] = new AnalistaRiesgos(2636363, "Analista experto", 18000.00, "Miguel", "Miramontes", "311-232-832", 34);
-        empleados[1] = new AgenteDeSeguros(false, 100, 55235225, "Nuevo", 7000.00, "Samuel", "Gomez", "311-534-821");
-        return empleados;
-    }
 }
