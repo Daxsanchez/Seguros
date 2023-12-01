310ef960552872f1f34247aa976985cbfadfa3cb
leafarsize <152042162+leafarsize@users.noreply.github.com>
2023-12-01 10:48:21 -0700

    subo

--- src/Principal/Funcionalidad.java
+++ src/Principal/Funcionalidad.java
@@ -13,34 +13,25 @@ import java.util.Date;
 import Renovaciones.Renovacion;
 import Renovaciones.RenovacionEstandar;
 import Renovaciones.RenovacionPlus;
+import Persona.AgenteDeSeguros;
+import Persona.AnalistaRiesgos;
+import Persona.Empleado;
+import Persona.ProcesarEmpleado;
 
 /**
  *
  * @author Dax Sánchez
  */
 public class Funcionalidad {
-
+    
     public void funcionesCliente() {
-        ClienteFrecuente[] c = new ClienteFrecuente[2];
-
-        c[0] = new Persona.ClienteFrecuente("Ruffino", "Perez", "311567908", 2);
-        c[1] = new Persona.ClienteFrecuente("Pedro", "Perez", "311567908", 5);
-        ProcesarCliente p = new ProcesarCliente();
-
-        p.showClientes(c);
-
-        Persona.ClienteNuevo[] clienteNuevo = new ClienteNuevo[2];
-        clienteNuevo[0] = new ClienteNuevo("Pedro", "Macias", "311-532-643", 7);
-        clienteNuevo[1] = new ClienteNuevo("Edgar", "Soto", "311-632-243", 3);
-        p.showClientes(clienteNuevo);
-
-        Persona.ClienteLeal[] clienteLeal = new ClienteLeal[2];
-        clienteLeal[0] = new ClienteLeal("Ernesto Lopez", "Mateos", "311-234-242", 12);
-        clienteLeal[1] = new ClienteLeal("José", "Huerta", "311-532-532", 10);
-
-        p.showClientes(clienteLeal);
+        ProcesarCliente clientes = new ProcesarCliente();
+        
+        Cliente[] cliente = clientes();
+        clientes.showClientes(cliente);
+        
     }
-
+    
     public Seguro[] seguros() {
         Seguro[] seguros = new Seguro[3];
         seguros[0] = new SeguroColision(5, "10014313", new Date(), new Date(), true,
@@ -51,7 +42,7 @@ public class Funcionalidad {
                 "Seguro contra robo", 6000.0, 50000.0);
         return seguros;
     }
-
+    
     public Renovacion[] renovaciones() {
         Renovacion[] renovaciones = new Renovacion[3];
         Seguro[] seguros = seguros();
@@ -61,12 +52,27 @@ public class Funcionalidad {
         renovaciones[2] = new RenovacionPlus("368", new Date(), seguros[2], "Transferencia", clientes[1]);
         return renovaciones;
     }
-
+    
     public Cliente[] clientes() {
         Cliente[] clientes = new Cliente[3];
         clientes[0] = new ClienteNuevo("Juan", "Sánchez", "31135643", 3);
         clientes[1] = new ClienteFrecuente("Pedro", "López", "31134354", 7);
         clientes[2] = new ClienteLeal("Edgar", "Hernández", "311354565", 15);
         return clientes;
+    }
+    
+    public void pruebaEmpleado() {
+        ProcesarEmpleado p = new ProcesarEmpleado();
+        Empleado[] empleados = empleados();
+        p.mostrarEmpleado(empleados);
+        
+    }
+    
+    private Empleado[] empleados() {
+        Empleado[] empleados = new Empleado[3];
+        empleados[0] = new AgenteDeSeguros(true, 300, 2134142, "Superior", 12000.00, "Arturo", "Bañuelos", "311-424-221");
+        empleados[1] = new AnalistaRiesgos(2636363, "Analista experto", 18000.00, "Miguel", "Miramontes", "311-232-832", 34);
+        empleados[1] = new AgenteDeSeguros(false, 100, 55235225, "Nuevo", 7000.00, "Samuel", "Gomez", "311-534-821");
+        return empleados;
     }
 }
